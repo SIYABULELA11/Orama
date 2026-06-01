ba07d01abc7b53d4576e461f2cc0a8f7e11410a1
Siyabulela Malinga <141419663+SIYABULELA11@users.noreply.github.com>
2026-06-01 21:21:14 +0200

    Switch from BrowserRouter to HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "./components/ui/toaster";
 import { Toaster as Sonner } from "./components/ui/sonner";
 import { TooltipProvider } from "./components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import MySchedules from "./pages/MySchedules";
@@ -18,8 +18,6 @@ import Support from "./pages/Support";
 import NotFound from "./pages/NotFound";
 import Login from "./pages/Login.tsx";
 
-import { Navigate } from "react-router-dom";
-
 const queryClient = new QueryClient();
 
 const App = () => (
@@ -27,7 +25,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Login />} />
 
@@ -48,7 +46,7 @@ const App = () => (
             <Route path="*" element={<NotFound />} />
           </Route>
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
